e83124d47b86cf7e9e44e81a6f475bc8eb878f8d
Felipe Bedana <felipe.bedana@hotmail.com>
2015-10-15 18:00:59 -0300

    Adicionado imagens e scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     public Vector2 speed = new Vector2(10, 10);
     public float scrollingspeed = 3f;
@@ -9,23 +10,32 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D m_Rigidbody;
     private Transform m_Transform;
     // Use this for initialization
-    void Awake () {
+    void Awake()
+    {
         m_Rigidbody = GetComponent<Rigidbody2D>();
         m_Transform = GetComponent<Transform>();
-	}
+    }
 
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
 
         movement = new Vector2((speed.x * inputX) + scrollingspeed, speed.y * inputY);
-	}
+    }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
-        
+
         m_Rigidbody.velocity = movement;
+    }
+
+    void OnTriggerEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Spawn")
+            Debug.Log("Mah oooii");
+
     }
 }
---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemy : MonoBehaviour {
+    [SerializeField]
+    private int m_NextEnemy = 1;
+    [SerializeField]
+    private int m_Enemy = 3;
+    [SerializeField]
+    private GameObject Inimigo;
+
+    private int m_SpawnedEnemies = 0;
+    private Transform m_Transform;
+    private bool m_SpawningEnemy = false;
+
+    private float m_ElapsedTime = 0.0f;
+     
+	// Use this for initialization
+	void Start () {
+        m_Transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(m_SpawningEnemy)
+        { 
+            m_ElapsedTime += Time.deltaTime;
+            if (m_ElapsedTime < m_NextEnemy)
+                return;
+            m_ElapsedTime = 0.0f;
+            m_SpawnedEnemies++;
+            if(m_SpawnedEnemies >= m_Enemy)
+            {
+                m_SpawningEnemy = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collision2D collider)
+    {
+        Debug.Log("Mah oooii");
+//        if (collider.gameObject.tag == "Player")
+//        {
+//            m_SpawningEnemy = true;
+//        }
+    }
+
+    void Spawn()
+    {
+        Instantiate(Inimigo, m_Transform.position, Quaternion.identity);
+    }
+}
+
