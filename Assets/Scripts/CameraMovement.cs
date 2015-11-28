de7de9797459a8b5a59c63b6009d97cb4db568ef
Felipe Bedana <felipe.bedana@hotmail.com>
2015-11-28 07:41:54 -0200

    Movimentação da nave inimiga

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,24 +2,15 @@
 using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
-    private Transform m_cameraposition;
-    private Transform m_playerposition;
-    public float tempolerp = 0.5f;
-	// Use this for initialization
-
-    void Start()
-    {
-        GameObject player = GameObject.FindGameObjectWithTag("PontoReferencia");
-        m_playerposition = player.GetComponent<Transform>();
-    }
-	void Awake () {
-        m_cameraposition = GetComponent<Transform>();
-    }
+    private float scrollspeed = 3f;
 	
 	// Update is called once per frame
-	void Update () {
-        Vector3 position = new Vector3(m_playerposition.position.x, 0,-10);
-        m_cameraposition.position = position;
+	void FixedUpdate () {
+        //float position = scrollspeed * Time.deltaTime;
+        //transform.Translate (new Vector3(position, 0, 0));
 
+        Debug.Log("XMin: " + Camera.main.rect.xMin + "\nYMin: " + Camera.main.rect.yMin);
+        Debug.Log("XMax: " + Camera.main.rect.xMax + "\nYMax: " + Camera.main.rect.yMax);
+        Debug.Log(Camera.main.ScreenToWorldPoint(Vector3.zero));
     }
 }
--- Assets/Scripts/EnemyShipMovement.cs
+++ Assets/Scripts/EnemyShipMovement.cs
@@ -2,18 +2,24 @@
 using System.Collections;
 
 public class EnemyShipMovement : MonoBehaviour {
-    float index;
+
+    [SerializeField]
+    private float horizontal_speed = -3f;
+
+    private float x;
+    private float y;
+
     [SerializeField]
     private float m_velocity_sine = 2;
 
     [SerializeField]
     private float m_limit = 5;
 
-    public void Update()
+    public void FixedUpdate()
     {
-        index += Time.deltaTime;
-        float x = 0f;
-        float y = Mathf.Sin(Time.time * m_velocity_sine);
-        transform.localPosition = new Vector3(x, y * m_limit, 0);
+        y = Mathf.Sin(Time.time * m_velocity_sine);
+        //transform.localPosition = new Vector3(x * Time.deltaTime, y * m_limit, transform.localPosition.z);
+        transform.Translate(new Vector3(horizontal_speed, y * m_limit, 0)*Time.deltaTime);
+
     }
 }
